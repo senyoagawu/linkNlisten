4e792852a1d06fede8d700d96ab6b57bc9e14678
sdkag <senyo.k.agawu@gmail.com>
2020-08-13 02:59:54 -0400

    end of day commit

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Configuration:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://facebook_app:password@localhost/faceboopk"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://facebook_app:password@localhost/facebook"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "klisafluiasdfiuh"
--- app/routes/sessions.py
+++ app/routes/sessions.py
@@ -7,7 +7,7 @@ from app.models import db, User
 from flask import Response
 import jwt  
 from ..config import Configuration
-from ..auth import require_auth
+# from ..auth import require_auth
 
 bp = Blueprint("sessions", __name__, url_prefix='/api/sessions')
 
@@ -16,6 +16,8 @@ bp = Blueprint("sessions", __name__, url_prefix='/api/sessions')
 # @cross_origin()
 def login():
     data = request.json
+    # em = data.email
+    print('=====', data, )
     user = User.query.filter(User.email == data['email']).first() #? email or Username for login
     if not user:
         return {"error": "Email not found"}, 422
@@ -28,7 +30,7 @@ def login():
         return {"error": "Incorrect password"}, 401
 
 # create new account
-@bp.route('/signup', methods=["POST"]) # create new account
+@bp.route('/signup', methods=["POST"],strict_slashes=False) # create new account
 # @cross_origin()
 def signup():
     data = request.json
@@ -42,7 +44,7 @@ def signup():
 
 # logout/delete session.
 @bp.route('/', methods=["DELETE"]) # is this really a delete method?
-@require_auth
+# @require_auth
 def logout():
     access_token = jwt.encode({'email': ''}, Configuration.SECRET_KEY)
     return {'access_token': access_token.decode('UTF-8'), 'user': ''}
