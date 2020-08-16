6a9b9229c636c80b166093d4b519fffac1a31dd4
sdkag <senyo.k.agawu@gmail.com>
2020-08-16 03:58:19 -0400

    finds interesting bug?

--- app/routes/posts.py
+++ app/routes/posts.py
@@ -6,14 +6,32 @@ bp = Blueprint("posts", __name__, url_prefix='/api/posts')
 
 @bp.route('/<string:email>/')  # fetch all posts of users in the same interests as user.
 def fetch_posts_with_follows(email):
+    """
+    fetches all the posts of users in the same
+    intereests as the current_user
+    orders entrys created_at timestamp
+    returns an array of post objs that contain the necessary user info
+    """
+
     user = User.find_by_email(email)
     subscribers = [i.subscribers for i in user.interests]
     subscribers_ids = [s.id for sub in subscribers for s in sub] # flatten the list
     unique_ids = list(set(subscribers_ids))
     posts = db.session.query(Post, User).join(User).filter(Post.id.in_(unique_ids)).order_by(Post.created_at.desc()).all()
+    res = [{**p.as_dict(), **{'author': u.as_dict()}} for p, u in posts]
+    # print([user.interests[0].subscribers])
+    # print(subscribers)
+    #! wtf????!!!!
+    #! user = User.find_by_email(email)
+    #! subscribers = [i.subscribers for i in user.interests]
+    #! flat_subs = [s for sub in subscribers for s in sub]
+    #! uniq_subs = list(set(flat_subs))
+    #! ps = [us.posts for us in uniq_subs]
+    #! flat_ps = [p for sub in ps for p in sub]
+    #! len(list(set(flat_ps)))
+    #! response = {}
+    # posts = [s.posts for s in [i.subscribers for i in user.interests]]
     
-    response = {}
-    res = [{**p.to_dict(), **{'author': u.as_dict()}} for p, u in posts]
     # for obj in res: 
     #     idx = obj['id']
     #     response[idx] = obj
