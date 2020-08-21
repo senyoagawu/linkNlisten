f21ca2b4e9e1e5d781f3b979bce89044b5b14aa7
sdkag <senyo.k.agawu@gmail.com>
2020-08-21 13:30:21 -0400

    update seeds

--- database.py
+++ database.py
@@ -7,38 +7,93 @@ from app.models import User, InterestUser, Interest, Post
 
 load_dotenv()
 fake = Faker()
-interests = ['Arts & Culture', 'Book Clubs', 'Career & Business', 'Cars & Motorcycles', 'Community & Environment', 'Dancing', 'Education & Learning', 'Fashion & Beauty', 'Fitness', 'Food & Drink', 'Games', 'Health & Wellbeing', 'Hobbies & Crafts', 'LGBT', 'Language & Ethnic Identity', 'Lifestyle', 'Movements & Politics', 'Movies & Film', 'Music', 'New Age & Spirituality', 'Outdoors & Adventure', 'Paranormal', 'Parents & Family', 'Pets & Animals', 'Photography', 'Religion & Beliefs', 'Sci-Fi & Fantasy', 'Singles', 'Socializing', 'Sports & Recreation', 'Support', 'Tech', 'Writing']
+interests = [
+    'Arts & Culture',
+    'Book Clubs',
+    'Career & Business',
+    'Cars & Motorcycles',
+    'Community & Environment',
+    'Dancing',
+    'Education & Learning',
+    'Fashion & Beauty',
+    'Fitness',
+    'Food & Drink',
+    'Games',
+    'Health & Wellbeing',
+    'Hobbies & Crafts',
+    'LGBT',
+    'Language & Ethnic Identity',
+    'Lifestyle',
+    'Movements & Politics',
+    'Movies & Film',
+    'Music',
+    'New Age & Spirituality',
+    'Outdoors & Adventure',
+    'Paranormal',
+    'Parents & Family',
+    'Pets & Animals',
+    'Photography',
+    'Religion & Beliefs',
+    'Sci-Fi & Fantasy',
+    'Singles',
+    'Socializing',
+    'Sports & Recreation',
+    'Support',
+    'Tech',
+    'Writing'
+]
 
 
 with app.app_context():
     db.drop_all()
     db.create_all()
 
-    demouser = User(first_name='demo',last_name='user', email='demo@gmail.com', hashed_password=generate_password_hash('password'), bio='stuff about stuff', location='location')
+    demouser = User(
+        first_name='demo',
+        last_name='user',
+        email='demo@gmail.com',
+        hashed_password=generate_password_hash('password'),
+        bio='stuff about stuff',
+        location='location'
+    )
+
     db.session.add(demouser)
     db.session.commit()
-        
+
     for a in range(5):
         u = User(
             first_name=fake.first_name(),
             last_name=fake.last_name(),
             email=fake.email(),
             password=generate_password_hash('password'),
-            )
+        )
+
         db.session.add(u)
         db.session.commit()
+
+    for i in range(len(interests)):
+        db.session.add(Interest(
+            name=interests[i],
+            created_at='now',
+            updated_at='now',
+            creators_id=(i % 5)+1
+        ))
         db.session.commit()
 
     for i in range(len(interests)):
-        db.session.add(Interest(name=interests[i], created_at='now', updated_at='now', creators_id=(i % 6)+1))
+        db.session.add(InterestUser(
+            users_id=(i % 5)+1,
+            interests_id=(i % 33)+1,
+            created_at='now',
+            updated_at='now'))
         db.session.commit()
-
-    for i in range(len(interests)):
-        db.session.add(InterestUser(users_id=u.id, interests_id=1+ a % 33, created_at='now', updated_at='now'))
-        db.session.commit()
-
 
     for interest in interests:
         for b in range(3):
-            db.session.add(Post(authors_id=u.id, body=fake.text(), created_at='now', updated_at='now'))
+            db.session.add(Post(
+                authors_id=(i % 5)+1,
+                body=fake.text(),
+                created_at='now',
+                updated_at='now'
+            ))
             db.session.commit()
