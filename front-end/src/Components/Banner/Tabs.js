15f1bb38c1c447f5691731a0f799d798be2bb615
sdkag <senyo.k.agawu@gmail.com>
2021-11-30 12:14:12 -0500

    sketch out Banner

--- front-end/src/Components/Banner/Tabs.js
+++ front-end/src/Components/Banner/Tabs.js
@@ -1,17 +1,25 @@
 import React, { useContext } from "react";
-import { TabContext } from "../../../Context/TabContext";
-export default function Tab() {
+import { TabContext } from "../../App";
+export default function Tabs() {
   const { selectedTab, setSelectedTab } = useContext(TabContext);
   const selectTab = (e) => setSelectedTab(e.target.id);
   return (
     <div className="tabs">
       <div className="tabs__title">
-        <span id="posts" onClick={selectTab}>
+        <span
+          className={selectedTab === "posts" ? "active" : ""}
+          id="posts"
+          onClick={selectTab}
+        >
           Posts
         </span>
       </div>
       <div className="tabs__title">
-        <span id="members" onClick={selectTab}>
+        <span
+          className={selectedTab === "members" ? "active" : ""}
+          id="members"
+          onClick={selectTab}
+        >
           Members
         </span>
       </div>
