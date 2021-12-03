87ac62fcab77bb7dd85eb12ab5a2afae16ec7907
sdkag <senyo.k.agawu@gmail.com>
2021-12-03 18:15:58 -0500

    to test redux action creators

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -13,11 +13,16 @@ import { getUsersList } from "./actions/auth";
 import { getSubscribedInterests, getPosts, getPost } from "./utils/ajax";
 import Navbar from "./Components/Navbar";
 import * as postActions from "./actions/posts";
+import * as interestActions from "./store/interests";
+import * as subscriptionActions from "./store/subscriptions";
 export const AppContext = createContext();
 export const TabContext = createContext();
 export const UIContext = createContext();
 export const PostContext = createContext();
 
+window.postActions = postActions;
+window.interestActions = interestActions;
+window.subscriptionActions = subscriptionActions;
 export const App = (props) => {
   const defaultUser = () =>
     localStorage.user ? JSON.parse(localStorage.user) : null;
